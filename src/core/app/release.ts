35bec786fed0685c61c8cd2ac4aad01887863e5b
h9k99rpg8n-cloud <h9k99rpg8n@privaterelay.appleid.com>
2026-08-14 15:48:59 -0600

    feat(texture-core): publish 0.0.4.0 release metadata

--- src/core/app/release.ts
+++ src/core/app/release.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = 'Addons Studio'
-export const APP_VERSION = '0.0.3.6.3'
+export const APP_VERSION = '0.0.4.0'
 export const APP_STAGE = 'Alpha'
-export const APP_RELEASE_NAME = 'Model Core Architecture & Stabilization'
+export const APP_RELEASE_NAME = 'Texture Core Foundation'
 
 export const APP_RELEASE_LABEL = `${APP_STAGE} ${APP_VERSION}`
