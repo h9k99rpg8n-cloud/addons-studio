3006ee1419073209d05b654ea2376e4167d7ff36
h9k99rpg8n-cloud <h9k99rpg8n@privaterelay.appleid.com>
2026-08-14 18:19:12 -0600

    Version Alpha 0.0.4.0.1 visual texture update

--- src/core/app/release.ts
+++ src/core/app/release.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = 'Addons Studio'
-export const APP_VERSION = '0.0.4.0'
+export const APP_VERSION = '0.0.4.0.1'
 export const APP_STAGE = 'Alpha'
-export const APP_RELEASE_NAME = 'Texture Core Foundation'
+export const APP_RELEASE_NAME = 'Visual Texture Workspace Update'
 
 export const APP_RELEASE_LABEL = `${APP_STAGE} ${APP_VERSION}`
